16d4d16b957018eca3e578080461672b704072ed
Arif B <arif.dev@pm.me>
2024-11-05 15:25:29 +0530

    Remove unnecessary config files

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,11 +1,5 @@
 return {
   {
-    "iamcco/markdown-preview.nvim",
-    config = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  },
-  {
     "OXY2DEV/markview.nvim",
     lazy = false, -- Recommended
     -- ft = "markdown" -- If you decide to lazy-load anyway
@@ -14,5 +8,25 @@ return {
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
     },
+  },
+  -- markdown preview
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    build = function()
+      require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+      vim.fn["mkdp#util#install"]()
+    end,
+    keys = {
+      {
+        "<leader>cp",
+        ft = "markdown",
+        "<cmd>MarkdownPreviewToggle<cr>",
+        desc = "Markdown Preview",
+      },
+    },
+    config = function()
+      vim.cmd([[do FileType]])
+    end,
   },
 }
