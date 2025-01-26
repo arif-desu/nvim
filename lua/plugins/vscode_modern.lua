f0577aec6339d9480db7e0174c5d9d06c0177971
Arif B <arif.dev@pm.me>
2025-01-26 14:25:31 +0530

    Simplify config

---
+++ lua/plugins/vscode_modern.lua
@@ -0,0 +1,10 @@
+return {
+  "gmr458/vscode_modern_theme.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    cursorline = true,
+    transparent_background = true,
+    nvim_tree_darker = true,
+  },
+}
