d49ce3e9dd018a12e0370076fd24d68f69418cf5
Arif B <arif.dev@pm.me>
2025-01-26 14:26:37 +0530

    Remove time

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -9,16 +9,11 @@ return {
       sections = {
         lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
         lualine_y = {
-          { "progress", separator = " ", padding = { left = 1, right = 0 } },
-          { "location", padding = { left = 0, right = 1 } },
+
+          { "progress", separator = " ", padding = { left = 1, right = 1 } },
         },
         lualine_z = {
-          {
-            function()
-              return " " .. os.date("%R")
-            end,
-            separator = { right = "" },
-          },
+          { "location", separator = { right = "" } },
         },
       },
     },
